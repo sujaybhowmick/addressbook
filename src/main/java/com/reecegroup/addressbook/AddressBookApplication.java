6e2156148fabba9cc164aaabc11aebfe893e414d
Sujay Bhowmick <sujay.bhowmick@catch.com.au>
2021-06-28 23:40:32 +1000

    Reformatted code and optimized imports

--- src/main/java/com/reecegroup/addressbook/AddressBookApplication.java
+++ src/main/java/com/reecegroup/addressbook/AddressBookApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AddressBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
 
 }
