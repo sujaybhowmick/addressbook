2e34253b0787babc1708022d63330a77c6aeefdd
Sujay Bhowmick <sujay.bhowmick@catch.com.au>
2021-07-01 15:44:53 +1000

    removed unique constraint for contact has column

--- src/main/resources/db/migration/V5__alter_table.sql
+++ src/main/resources/db/migration/V5__alter_table.sql
@@ -1,5 +1,3 @@
-alter table address_books add constraint uk_8umo2vy6ox65yln40x8s1lqx2 unique (address_book_name);
-alter table contacts add constraint uk_3sm3vvcgbb4xdhi6nvjmgnhd8 unique (contact_hash);
 alter table users add constraint uk_k8d0f2n7n88w1a16yhua64onx unique (user_name);
 alter table address_books add constraint fk2dj8ohycf9g1lf18ypkcy8vxw foreign key (owner_id) references users;
 alter table contacts add constraint fk53x4ihl1wmiriun7wj3vvlis0 foreign key (address_book_id) references address_books;
